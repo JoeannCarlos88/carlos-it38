8f8aa07f2cf44236207dfa8cb7e0b9beb115256d
JoeannCarlos88 <143687489+JoeannCarlos88@users.noreply.github.com>
2025-02-06 07:59:52 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'carlos IT38',
+  webDir: 'dist'
+};
+
+export default config;
